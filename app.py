852d557c0af696ceb0fbc67e76398471ea8d6a5c
Kausar Ahmed <kausarahmed2nd@gmail.com>
2025-03-04 01:25:13 +0600

    added general login page and routes for other pages

--- app.py
+++ app.py
@@ -8,5 +8,35 @@ def home():
     return render_template("index.html")
 
 
+@app.route("/login")
+def login_general():
+    return render_template("login_general.html")
+
+
+@app.route("/user_login")
+def login_user():
+    return render_template("user_signup.html")
+
+
+@app.route("/user_signup")
+def signup_user():
+    return render_template("user_signup.html")
+
+
+@app.route("/company_login")
+def login_company():
+    return render_template("company_login.html")
+
+
+@app.route("/company_signup")
+def signup_company():
+    return render_template("company_signup.html")
+
+
+@app.route("/find_bins")
+def find_bins():
+    return render_template("find_bin.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
