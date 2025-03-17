1ae5cc4abda3c7af6d803d8fc7ef98182a99163b
Kausar Ahmed <kausarahmed2nd@gmail.com>
2025-03-18 02:21:51 +0600

    added the user login, signup and dashboard route

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify, render_template, session, redirect, url_for
-import mysql.connector 
+import mysql.connector
+from functools import wraps
 
 app = Flask(__name__)
 
@@ -14,6 +15,17 @@ def get_db_connection():
     return connection
 
 
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if "loggedin" not in session:
+            print("Please log in to access this page.")
+            return redirect(url_for("general.login_general"))
+        return f(*args, **kwargs)
+
+    return decorated_function
+
+
 @app.route("/", methods=["GET"])
 def home():
     return render_template("index.html")
@@ -24,14 +36,93 @@ def login_general():
     return render_template("login_general.html")
 
 
-@app.route("/user_login")
-def login_user():
+@app.route("/user_login", methods=["POST", "GET"])
+def user_login():
+    if request.method == "POST":
+        email = request.form["email"]
+        password = request.form["password"]
+
+        try:
+            conn = get_db_connection()
+            cursor = conn.cursor(dictionary=True)
+            cursor.execute("SELECT * FROM user WHERE user_email = %s", (email,))
+            account = cursor.fetchone()
+
+            if account and account["user_password"] == password:
+                session.update(
+                    {
+                        "loggedin": True,
+                        "id": account["user_id"],
+                        "name": account["user_name"],
+                        "email": account["user_email"],
+                        "location": account["user_location"],
+                        "date": account["user_joining_date"],
+                    }
+                )
+                print("Login successful!")
+                return redirect(url_for("user_dashboard"))
+            else:
+                print("Incorrect username/password!")
+
+        except Exception as e:
+            print(f"Error: {e}")
+
+        finally:
+            conn.close()
+
     return render_template("user_login.html")
 
 
-@app.route("/user_signup")
-def signup_user():
+@app.route("/user_signup", methods=["POST", "GET"])
+def user_signup():
+    if request.method == "POST":
+        name = request.form["name"]
+        location = request.form["location"]
+        email = request.form["email"]
+        password = request.form["password"]
+
+        try:
+            conn = get_db_connection()
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM user WHERE user_email = %s", (email,))
+            account = cursor.fetchone()
+
+            if account:
+                print("Account already exists!")
+            else:
+                cursor.execute(
+                    "INSERT INTO user (user_name, user_email, user_password, user_location) VALUES (%s, %s, %s, %s)",
+                    (name, email, password, location),
+                )
+                print("Account created successfully!")
+                conn.commit()
+
+        except Exception as e:
+            print(f"Error: {e}")
+
+        finally:
+            conn.close()
+
+        return redirect(url_for("user_login"))
+
     return render_template("user_signup.html")
+
+
+@app.route("/user_dashboard", methods=["POST", "GET"])
+@login_required
+def user_dashboard():
+    name = session.get("name")
+    location = session.get("location")
+    email = session.get("email")
+    date = session.get("date")
+
+    if session.get("loggedin"):
+        return render_template(
+            "user_dashboard.html", name=name, location=location, email=email, date=date
+        )
+
+    else:
+        return redirect(url_for("user_login"))
 
 
 @app.route("/company_signup", methods=["POST", "GET"])
@@ -41,8 +132,7 @@ def company_signup():
         company_location = request.form["company_location"]
         email = request.form["email"]
         password = request.form["password"]
-        print("Hello")
-
+        
         try:
             conn = get_db_connection()
             cursor = conn.cursor()
@@ -105,7 +195,6 @@ def company_login():
     return render_template("company_login.html")
 
 
-
 @app.route("/company_dashboard", methods=["POST", "GET"])
 @login_required
 def company_dashboard():
@@ -116,7 +205,11 @@ def company_dashboard():
 
     if session.get("loggedin"):
         return render_template(
-            "company_dashboard.html", company_name=company_name, company_email=company_email, company_location = company_location, company_date=company_date
+            "company_dashboard.html",
+            company_name=company_name,
+            company_email=company_email,
+            company_location=company_location,
+            company_date=company_date,
         )
     else:
         return redirect(url_for("company_login"))
